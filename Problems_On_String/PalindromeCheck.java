9503f9c69e929b3ef8fb7cf61285cf6f025f90f4
Bidyajani Maji <bidyajanimaji2003@gmail.com>
2025-10-29 18:07:27 +0530

    stringPalindromChechSolve

--- Problems_On_String/PalindromeCheck.java
+++ Problems_On_String/PalindromeCheck.java
@@ -1,5 +1,18 @@
 package Problems_On_String;
-
+import java.util.*;
 public class PalindromeCheck {
-    
+    Scanner sc  = new Scanner(System.in);
+        String str = sc.next();
+        int start = 0;
+        int end = str.length() - 1;
+        while(start <= end){
+            if(str.charAt(start) == str.charAt(end)){
+                start++;
+                end--;
+            } else{
+                System.out.println("not a palindrom");
+                return;
+            }
+        }
+        System.out.println("is a palindrom");
 }
